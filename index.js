45d4ecb79aba01161469e11f82b4853c3ba29bfc
sulemanahsancui <sulemanahsan12345@gmail.com>
2021-11-29 17:37:25 +0500

    ok

--- index.js
+++ index.js
@@ -11,7 +11,12 @@ app.use(express.json());
 app.use(cors());
 
 //routes
-
+const data = {
+  name: "chicken",
+  price: 420,
+  img: "https://media.istockphoto.com/photos/chicken-isolated-on-white-picture-id148127611?k=20&m=148127611&s=612x612&w=0&h=xQmE1alpIqvVGp9DcvLV8UJ--_xrPauc4Z5R588kZTg=",
+  stock: "44",
+};
 //Create items
 app.post("/api/items", async (req, res) => {
   const data = req.body;
