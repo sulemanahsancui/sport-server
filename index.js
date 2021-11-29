e1adb56ac49e6a3676936a591d1cc08314cd5e8a
sulemanahsancui <sulemanahsan12345@gmail.com>
2021-11-29 17:42:10 +0500

    done

--- index.js
+++ index.js
@@ -19,38 +19,38 @@ const data = {
 };
 //Create items
 app.post("/api/items", async (req, res) => {
-  const data = req.body;
-  const new_product = await new product(data);
-  const items = await new_product.save();
-  res.status(200).send({ items });
+  // const data = req.body;
+  // const new_product = await new product(data);
+  // const items = await new_product.save();
+  res.status(200).send({ data });
 });
 
 //GET ALL ITEMS GET
 app.get("/api/items", async (req, res) => {
-  const products = await product.find();
-  res.status(200).send(products);
+  // const products = await product.find();
+  res.status(200).send(data);
 });
 
 //Sign in user
 
-app.post("/api/users", async (req, res) => {
-  const user = req.body;
-  const user_email = await User.findBYEmail(user.email);
-  if (!user_email) {
-    const insert_user = await user.save();
-  } else {
-    res.status(404).send({ message: "user Exist try another mail" });
-  }
-});
+// app.post("/api/users", async (req, res) => {
+//   const user = req.body;
+//   const user_email = await User.findBYEmail(user.email);
+//   if (!user_email) {
+//     const insert_user = await user.save();
+//   } else {
+//     res.status(404).send({ message: "user Exist try another mail" });
+//   }
+// });
 
-app.get("/api/users", (req, res) => {});
+// app.get("/api/users", (req, res) => {});
 
 //initialize server
 const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
-  mongoose.connect("mongodb://localhost/products", () => {
-    console.log("connected");
-  });
+  // mongoose.connect("mongodb://localhost/products", () => {
+  //   console.log("connected");
+  // });
   console.log(`server is running at port ${port}`);
 });
